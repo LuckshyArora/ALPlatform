754804778e70269457cdfbeda769db4b2fbb47b8
LuckshyArora <luckshyaarora@gmail.com>
2025-02-18 15:41:30 +0530

    Create chatbot.py
    Created class for chatbot.

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+class Chatbot:
+    def __init__(self):
+        # will have to initialize chatbot engine with NLP and ML models
+        pass
+
+    def respond(self, user_input):
+        # will implement logic to generate response based on user input
+        pass
