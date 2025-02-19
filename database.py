c820d5c85557d9c990ffe58edf8a2fbfb97f614c
LuckshyArora <luckshyaarora@gmail.com>
2025-02-19 12:05:14 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,12 @@
+class Database:
+    def __init__(self):
+        # will initialize database connection with data storage solution (MongoDB or SQL)
+        pass
+
+    def store_data(self, data):
+        # will implement logic to store data in database
+        pass
+
+    def retrieve_data(self, query):
+        # will implement logic to retrieve data from database
+        pass
